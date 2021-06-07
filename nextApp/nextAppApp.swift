0b42d889b5346442a0cdcb99f6629aa1db3601e6
starosta64 <kro00@mail.ru>
2021-06-07 16:18:04 +0300

    Initial Commit

---
+++ nextApp/nextAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  nextAppApp.swift
+//  nextApp
+//
+//  Created by user on 07.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct nextAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
